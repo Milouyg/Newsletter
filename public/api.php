4e6e8bdd84d334e842cf5e6414c7be431cc0a212
Milouyg <33333@ma-web.nl>
2023-10-12 10:00:06 +0200

    Connection with the database is succesfull

--- public/api.php
+++ public/api.php
@@ -8,18 +8,20 @@ if(empty($_POST['email'])){
     $email = validation("email");
     echo "Je $fname, $lname en $email is gelukt";
 
-    $servername = "localhost";
+    $servername = "mariadb";
     $user = "milou";
     $password = "createApi";
     $db = "newsletters";
+    $dateTime = new DateTime();
+    $dateNow = $dateTime->format('Y-m-d H:i:s'); 
 
     try{
         $conn = new PDO("mysql:host=$servername;port=3306;dbname=$db", $user, $password);
         $stmt = $conn->prepare("INSERT INTO user(email, active, create_date, first_name, last_name) VALUES(:email, :active, :create_date, :first_name, :last_name)");
         $stmt->execute([
             ":email" => $email,
-            ":active" => false,
-            ":create_date" => new DateTime("now"),
+            ":active" => 0,
+            ":create_date" => $dateNow,
             ":first_name" => $fname,
             ":last_name" => $lname
         ]);
